99bd71b8628ff5e07072c192c949917d143b83e7
Vadim Plakhtinskii <v.plakhtinskiy@corp.vk.com>
2020-09-21 23:49:50 +0300

    generate dir with coverage result

--- test-analyzer/get-coverage.py
+++ test-analyzer/get-coverage.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import shutil
 
 
 def get_test_files(dir_with_tests):
@@ -11,15 +12,23 @@ def get_test_files(dir_with_tests):
     return test_files
 
 
-def generate_coverage_file(src_path, test_file_path):
+def generate_coverage_file(src_path, test_file_path, test_name, coverage_dir_path):
+    print(test_file_path)
     run_pytest_for_file = "coverage run -m --source={}  pytest {}".format(src_path, test_file_path)
     subprocess.call(run_pytest_for_file, shell=True)
-    get_coverage = "coverage json --include={}/* --pretty-print -o {}.coverage".format(src_path, test_file_path)
+    get_coverage = "coverage json --include={}/* --pretty-print -o {}/{}.coverage".format(src_path, coverage_dir_path, test_name)
     subprocess.call(get_coverage, shell=True)
 
 
 def get_coverage(src_path, dir_with_tests):
+    coverage_dir_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "coverage")
+    if os.path.exists(coverage_dir_path):
+        shutil.rmtree(coverage_dir_path)
+
+    os.mkdir(coverage_dir_path)
+
     test_files = get_test_files(dir_with_tests)
     for test_file in test_files:
-        generate_coverage_file(src_path, "{}/{}".format(dir_with_tests, test_file))
+        generate_coverage_file(src_path, "{}/{}".format(dir_with_tests, test_file), test_file, coverage_dir_path)
 
+get_coverage("../simple", "./..") ## TODO: edit this paths
